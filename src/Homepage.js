8773d9e62103acc7d2e62187e4c6e64b01c2ac14
CASEY GUNGEON <bound2theground@gmail.com>
2024-03-23 17:26:47 -0700

    fix itch link, add experience text

--- src/Homepage.js
+++ src/Homepage.js
@@ -39,14 +39,14 @@ const Homepage = () => {
 					<div className="center">
 						<br /><br /><br />
 						<Box sx={{ backgroundColor: 'black', width:'50%', margin:'auto'}}>
-							<p>I'm a 27-year-old full-stack developer who uses JavaScript with React. (It's how I built this website!) I also have experience working in fast-paced IT support both with a ticket queue and in realtime, and I love to design and program video games!</p>
+							<p>I'm a 27-year-old full-stack developer with experience in Java and React. (It's how I built this website!) I also have experience working in fast-paced IT support both with a ticket queue and in realtime, and I love to design and program video games! I primarily work in the Unity engine, but have also used GameMaker Studio and BYOND.</p>
 							<p>If you're seeing this page, there's a decent chance that it's because I'm looking for a job. I'm ready to work right away, so please hire me! If you don't have my e-mail address from my resume, feel free to contact me at <a style={{ color: "yellow" }} href="mailto:mail@caseygungeon.com">mail@caseygungeon.com</a></p>
 						</Box>
 					</div>
 				</div>
 				<footer className="center">
 					<a style={{color:"white"}} href="https://github.com/CASEYGUNGEON" title='Github'><GitHubIcon style={{fontSize: 50}}/></a>
-					<a style={{color:"red"}} href="https://itch.io/CASEYGUNGEON" title='itch.io'><StorefrontIcon style={{fontSize: 50}}/></a>
+					<a style={{color:"red"}} href="https://caseygungeon.itch.io/" title='itch.io'><StorefrontIcon style={{fontSize: 50}}/></a>
 					<a style={{color:"gray"}} href="https://toyhou.se/CASEYGUNGEON/characters" title='Toyhou.se'><HouseIcon style={{fontSize: 50}}/></a>
 				</footer>
 			</div>
